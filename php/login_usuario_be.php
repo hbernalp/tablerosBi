01d19432ce279194fbb77cf925cdda01729b4093
Harrisson Bernal <47458411+hbernalp@users.noreply.github.com>
2022-04-20 11:11:36 -0500

    prueba

--- php/login_usuario_be.php
+++ php/login_usuario_be.php
@@ -11,6 +11,7 @@ $contrasena = $_POST['contrasena'];
 $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo' and contrasena = '$contrasena' ");
 
 if (mysqli_num_rows($validar_login) > 0){
+
     $_SESSION['usuario'] = $correo;
     header("location: ../bienvenido.php");
     exit();

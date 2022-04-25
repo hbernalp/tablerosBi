97d712cd07a78cb51f4f42e07623415dac2649d3
Harrisson Bernal <47458411+hbernalp@users.noreply.github.com>
2022-04-25 12:47:59 -0500

    Delete plantillas_xls directory
    Borrado de la carpeta, contiene info no importante para el proyecto

--- plantillas_xls/roles.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `rol` (`id`, `id_perfil`, `asi_vamos`, `flash_comercial`, `supply`, `crm`, `crm_rfm`, `financiero`, `rrss_cromantic`, `influenciadoras_cromantic`, `rrss_blind`, `influenciadoras_blind`, `rotacion`, `wep`, `ecommerce_analytics`, `pauta_cromantic`, `pauta_blind`, `audiencia_cromantic`, `audiencia_blind`, `apps_mkp`, `marcas_propias`, `demografico`, `formacion`, `edad_inventario`, `sac_blind`, `sac_cromantic`) VALUES 
-                     (NULL, '101', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
-
-
-
-INSERT INTO `url` (`id`, `cod`, `nombre`, `direccion`, `id_perfil`) VALUES 
-                  (NULL, '1020', 'Supply', 'https://app.powerbi.com/view?r=eyJrIjoiNGQ3ZTcwMTMtZWY2Mi00YTc4LTgzYzYtMTdkZTE4NDJhYzc2IiwidCI6IjNmOTU5M2UxLTU1ZGEtNDQyZi05M2RkLTU3Y2NmNzVmNGI3OCIsImMiOjR9', '102'), (NULL, '1021', 'Edad Inventario', 'https://app.powerbi.com/view?r=eyJrIjoiMmU3OWE0YWMtOGVjMy00NDU1LTliZmMtZWRlYzNjYzI2OTg0IiwidCI6IjNmOTU5M2UxLTU1ZGEtNDQyZi05M2RkLTU3Y2NmNzVmNGI3OCIsImMiOjR9', '102');

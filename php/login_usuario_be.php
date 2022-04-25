f073381e9680f411ef014e4ec7ee53777b44e506
Harrisson Bernal <47458411+hbernalp@users.noreply.github.com>
2022-04-25 12:45:39 -0500

    Inicia la creacion de las vistas

--- php/login_usuario_be.php
+++ php/login_usuario_be.php
@@ -5,10 +5,11 @@ include 'conexion_be.php';
 
 $correo = $_POST['correo'];
 $contrasena = $_POST['contrasena'];
+
 //$contrasena = hash('sha512', $contrasena);
 
 
-$validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo' and contrasena = '$contrasena' ");
+$validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo' and contrasena = '$contrasena'");
 
 if (mysqli_num_rows($validar_login) > 0){
 
